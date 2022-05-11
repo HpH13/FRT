0b4559b71d39be634d597d165de17683296a06b2
HpH13 <102658614+HpH13@users.noreply.github.com>
2022-05-11 20:19:54 +0100

    Update Face_Recog.py

--- Face_Recog.py
+++ Face_Recog.py
@@ -7,7 +7,7 @@ from PIL import Image #Pillow lib for handling images
 
 led = LED(23)
 
-labels = ["Harry", "Gerrard",] #Names of people
+labels = ["Harry", "Em",] #Names of people
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') #Haarcascade classifier
 recognizer = cv2.face.createLBPHFaceRecognizer()
@@ -16,7 +16,7 @@ recognizer.load("face-trainner.yml") #Load YML file created from face traniner
 cap = cv2.VideoCapture(0) #Get vidoe feed from the Camera
 
 while(True):
-    led.off()
+    led.off() #LED is off
     ret, img = cap.read() # Break video into frames 
     gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert Video frame to Greyscale
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5, ) #Recognize faces
@@ -30,20 +30,20 @@ while(True):
             font = cv2.FONT_HERSHEY_SIMPLEX #Font style
             name = labels[id_] #Acquire name from the List using ID number
             confidence = "{0}%".format(round(100 - confidence)) #confidence rating 
-            led.on()
+            led.on() #LED turns on if face is a match
             
             cv2.putText(img, name, (x+5,y-5), font, 1, (255,255,255), 2) #Place name around face
             cv2.putText(img, str(confidence), (x+5,y+h-5), font,1, (225,225,0), 2) #Place confidence rating around face
             cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2) #Place box around face
         else:
-            name = "Unkown"
+            name = "Unkown" #Place name around face 
             font = cv2.FONT_HERSHEY_SIMPLEX #Font style
-            confidence = "{0}%".format(round(100 - confidence))
-            led.off()
+            confidence = "{0}%".format(round(100 - confidence)) #Confidence rating
+            led.off() #LED off
             
-            cv2.putText(img, name, (x+5,y-5), font, 1, (255,255,255), 2)
-            cv2.putText(img, str(confidence), (x+5,y+h-5), font,1, (225,225,0), 2)
-            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(img, name, (x+5,y-5), font, 1, (255,255,255), 2) #Place name around face
+            cv2.putText(img, str(confidence), (x+5,y+h-5), font,1, (225,225,0), 2) #place confidence rating around face
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2) #place box around face 
 
     cv2.imshow('Preview',img) #Display the Video
     if cv2.waitKey(20) & 0xFF == ord('q'):
